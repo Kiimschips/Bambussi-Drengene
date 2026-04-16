afcaa174aab4a2e91c0f5c9eaadf5fdfcb15daf0
MarcusBach44 <mbach22@student.aau.dk>
2026-04-16 15:38:41 +0200

    test

--- app/src/main/java/com/example/bambussi/MainActivity.java
+++ app/src/main/java/com/example/bambussi/MainActivity.java
@@ -1,32 +1,70 @@
 package com.example.bambussi;
 
+import static android.content.Intent.FLAG_ACTIVITY_REORDER_TO_FRONT;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewParent;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.activity.OnBackPressedDispatcher;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static int instances = 0;
+    private int id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button changeButton = (Button) findViewById(R.id.button);
+        changeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChangeToNFCReader();
+            }
         });
+
+        findViewById(R.id.button3).setOnClickListener(v ->{
+            Toast.makeText(MainActivity.this, "ID: " + id, Toast.LENGTH_SHORT).show();
+        });
+
+        instances++;
+        id = instances;
     }
 
-
-
-    public void ChangeToNFCReader(View v){
-        startActivity(new Intent(this, NFCReader.class));
+    public void ChangeToNFCReader() {
+        Intent intent = new Intent(this, NFCReader.class);
+        intent.setFlags(FLAG_ACTIVITY_REORDER_TO_FRONT);
+        startActivity(new Intent(intent));
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "Main - Paused", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Main"+id+" - Destroyed", Toast.LENGTH_SHORT).show();
+    }
+
 }
+
