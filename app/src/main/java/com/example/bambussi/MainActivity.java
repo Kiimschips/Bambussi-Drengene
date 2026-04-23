8e218fac57ee2369bd6d1a152f9648d7d6ad84ad
MarcusBach44 <mbach22@student.aau.dk>
2026-04-23 13:57:36 +0200

    fix

--- app/src/main/java/com/example/bambussi/MainActivity.java
+++ app/src/main/java/com/example/bambussi/MainActivity.java
@@ -1,70 +1,28 @@
 package com.example.bambussi;
 
-import static android.content.Intent.FLAG_ACTIVITY_REORDER_TO_FRONT;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.ViewParent;
-import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.activity.OnBackPressedCallback;
-import androidx.activity.OnBackPressedDispatcher;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static int instances = 0;
-    private int id;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        Button changeButton = (Button) findViewById(R.id.button);
-        changeButton.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.btnBattle).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ChangeToNFCReader();
             }
         });
-
-        findViewById(R.id.button3).setOnClickListener(v ->{
-            Toast.makeText(MainActivity.this, "ID: " + id, Toast.LENGTH_SHORT).show();
-        });
-
-        instances++;
-        id = instances;
     }
-
-
-
-    public void ChangeToNFCReader(View v){
+    public void ChangeToNFCReader(){
         startActivity(new Intent(this, Simple_Battle.class));
     }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(this, "Main - Paused", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this, "Main"+id+" - Destroyed", Toast.LENGTH_SHORT).show();
-    }
-
 }
 
